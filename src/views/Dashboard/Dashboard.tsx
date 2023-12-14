28ef698e71b9aa1cf348986d8ec33c34e678b58c
Krzysztof <krzysiekksi6@gmail.com>
2023-12-14 04:34:58 +0100

    feature/m basic styles

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,31 +1,56 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import {
   selectUserDetails,
   selectUserId,
 } from "src/components/features/auth/authSlice";
+import {
+  UserDetailsWrapper,
+  UserDetailSection,
+  EditLink,
+  Wrapper,
+} from "./Dashboard.styles";
 
-import { Wrapper } from "../Root/Root.styles";
 import MainTemplate from "src/templates/MainTemplate/MainTemplate";
+import { AverageBMI } from "src/components/atoms/AverageBMI/AverageBMI.styles";
+import { ArticleWrapper } from "src/templates/NewsSection/NewsSection.style";
+import { AverageBMR } from "src/components/atoms/AverageBMR/AverageBMR.styles";
 const Dashboard = () => {
   const userDetails = useSelector(selectUserDetails);
   const userId = useSelector(selectUserId);
   return (
     <MainTemplate>
-      <Wrapper>
+      <UserDetailsWrapper>
         {userDetails ? (
           <>
-            <p>BMI: {userDetails.bmi}</p>
-            <p>BMR: {userDetails.bmr}</p>
-            <Link to={`/auth/details/${userId}/edit`}>Edytuj swoje dane</Link>
+            <Wrapper>
+              <UserDetailSection>
+                <AverageBMI value={userDetails.bmi}>
+                  <p>BMI: </p>
+                  <p>{userDetails.bmi}</p>
+                </AverageBMI>
+                <AverageBMR>
+                  <p>BMR:</p>
+                  <p>{userDetails.bmr}</p>
+                </AverageBMR>
+              </UserDetailSection>
+              <EditLink to={`/auth/details/${userId}/edit`}>
+                Edytuj swoje dane
+              </EditLink>
+            </Wrapper>
           </>
         ) : (
           <>
-            <p>Brak danych o użytkowniku</p>
-            <Link to={`/auth/details`}>Dodaj szczegóły</Link>
+            <UserDetailSection>
+              <NoDataText>Brak danych o użytkowniku</NoDataText>
+            </UserDetailSection>
+            <UserDetailSection>
+              <AddDetailsLink to={`/auth/details`}>
+                Dodaj szczegóły
+              </AddDetailsLink>
+            </UserDetailSection>
           </>
         )}
-      </Wrapper>
+      </UserDetailsWrapper>
     </MainTemplate>
   );
 };
